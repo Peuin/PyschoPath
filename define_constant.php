f13909d920e98ce45b2a1f02c752bbbea6524e15
Peuin <57762484+Peuin@users.noreply.github.com>
2021-07-14 19:28:40 +0700

    Update define_constant.php

--- define_constant.php
+++ define_constant.php
@@ -1,10 +1,10 @@
 <?php
 /************ Database ************/
 
-define('CMS_DB_HOST', 'd9c88q3e09w6fdb2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
-define('CMS_DB_USER', 'hjpt0qhmgqu0q26a');
-define('CMS_DB_PASSWORD', 'ovnkpgl653iw351y');
-define('CMS_DB_NAME', '	fd94usxu1qbyxpcj');
+define('CMS_DB_HOST', 'ryvdxs57afyjk41z.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('CMS_DB_USER', 'zlcazhfxag983emz');
+define('CMS_DB_PASSWORD', 'weh30iib661cntkj');
+define('CMS_DB_NAME', 'azpzvdgepi9y3h16');
 define('CMS_DB_PREFIX', 'cms_');
 
 /************ ORTHER ************/
